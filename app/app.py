35c4c090c96712d5c62e2aa04dd4c3f3654f5d48
tiagomartins <tiago.n.m.martins@gmail.com>
2024-01-30 12:44:03 +0000

    Checkpoint Tuesday

--- app/app.py
+++ app/app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.preprocessing import PowerTransformer
+st.set_page_config(layout="centered")
 
 cost_of_living = pd.read_csv('../data/cleaned/cost_of_living_cleanedV1.csv')
 
 def input_data_streamlit(cost_of_living_dataframe):
     # Values for dropdown menus
     columns = {
-        'work_year': [2020, 2021, 2022, 2023, 2024, 2025],
-
         'employee_residence': ['Germany', 'United States', 'United Kingdom', 'Canada', 'Spain',
        'Ireland', 'South Africa', 'Poland', 'France', 'Czech Republic',
        'Netherlands', 'Pakistan', 'Ukraine', 'Lithuania', 'Portugal',
@@ -52,22 +52,24 @@ def input_data_streamlit(cost_of_living_dataframe):
         'job_field': ['Data Engineering', 'Data Science', 'Data Analysis', 'Other']
     }
 
-    # Dictionary to save the inputs
+    # Creating a dictionary to save the inputs
     data = {}
 
-    # Use Streamlit widgets to get user input
+    # creating dropdown menus
     for column, options in columns.items():
-        if options:  # If options are provided, use a select box
-            data[column] = st.selectbox(f"Select {column}:", options)
-        else:  # For numerical input
-            data[column] = st.number_input(f"Enter {column}:", step=1.0)
+        column_formatted = column.replace("_", " ")
+        if options:
+            data[column] = st.selectbox(f"Select {column_formatted}:", options)
+        else:
+            data[column] = st.number_input(f"Enter {column_formatted}:", step=1.0)
 
-    # Button to add data to DataFrame
+    # actions after pressing the button predict
     if st.button('Predict'):
         new_row = pd.DataFrame([data], columns=columns.keys())
 
-        # Perform the merge operation here, after the new entry is added
+        # Performing the merge operation
         df_merged = new_row.merge(cost_of_living_dataframe, left_on='employee_residence', right_on='country', how='left').drop(columns='country')
+        
         experience_level = {
         'Entry-level': 1,
         'Mid-level': 2,
@@ -97,27 +99,99 @@ def input_data_streamlit(cost_of_living_dataframe):
         df_num = df_merged.select_dtypes(np.number)
         df_cat = df_merged.select_dtypes(object)
 
+        #loading saved power transformer for the X
+        pickle_file_path = '../ml/transformers/power_transformer_x.pkl'
+        with open(pickle_file_path, 'rb') as file:
+            pt_x = pickle.load(file)
 
-        # Importing scaler I used in the model
-        scaler_file = '../ml/scalers/standard_scaler.pkl'
+        #fitting and transforming the numerical df using the saved power transformer
+        df_num_pt = pt_x.transform(df_num)
 
-        with open(scaler_file, 'rb') as file:
-            loaded_scaler = pickle.load(file)
+        #creating transformed dataframe
+        df_num_transformed = pd.DataFrame(df_num_pt, columns=df_num.columns , index=df_num.index)
 
-        # loading the scaler on the user dataframe
-        scaled_data = loaded_scaler.transform(df_num)
-        sc_df = pd.DataFrame(X_train_num_transformed, columns=X_train_num.columns , index=X_train_num.index)
 
-        return scaled_data
+        #loading one hot encoder
+        pickle_file_path = '../ml/encoders/one_hot_encoder.pkl'
+        with open(pickle_file_path, 'rb') as file:
+            ohe = pickle.load(file)
 
+        #fitting and transforming the categorical df using the saved one hot encoder
+        df_cat_ohe = ohe.transform(df_cat).toarray()
+
+        #creating transformed dataframe
+        encoded_feature_names = ohe.get_feature_names_out(df_cat.columns)
+        df_cat_encoded = pd.DataFrame(df_cat_ohe, columns=encoded_feature_names, index=df_cat.index)
+
+        #concatenating dataframes
+        df_concat = pd.concat([df_num_transformed, df_cat_encoded], axis=1)
+
+        #loading the saved min max scaler
+        pickle_file_path = '../ml/scalers/MinMaxScaler_concat.pkl'
+        with open(pickle_file_path, 'rb') as file:
+            mms = pickle.load(file)
+
+        #applying another Min Max scaler to the dataframe
+        concat_scaled = mms.transform(df_concat)
+
+        df_concat_scaled = pd.DataFrame(concat_scaled, columns=df_concat.columns , index=df_concat.index)
+
+        #loading the saved model
+        pickle_file_path = '../ml/models/gradient_boost_regressor.pkl'
+        with open(pickle_file_path, 'rb') as file:
+            gb_reg = pickle.load(file)
+
+        # predicting the transformed y
+        y_pred_transf = gb_reg.predict(df_concat_scaled)
+
+        #loading the power transformer for the y
+        pickle_file_path = '../ml/transformers/power_transformer_y.pkl'
+        with open(pickle_file_path, 'rb') as file:
+            pt_y = pickle.load(file)
+
+        #reshapping the transformed data
+        y_train_2d = y_pred_transf.reshape(-1, 1)
+        y_pred = pt_y.inverse_transform(y_train_2d)
+
+        #extracting and rounding the predicted value
+        salary = round ((y_pred[0][0])/100) * 100
+
+        #output header
+        st.write('The expected salary for the selected features is approximately :')
+        # st.write(f'{user_df} €')
+
+        #output target
+        st.markdown("""
+        <style>
+        .big-font {
+            font-size:50px !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+        st.markdown(f'<p class="big-font">{salary} €</p>', unsafe_allow_html=True)
 
     # Return an empty DataFrame if button is not pressed
     return pd.DataFrame(columns=columns.keys())
 
 # Use the function in Streamlit
-st.title('Jobs in Data salary predictor')
+st.title('Jobs in Data - Wage Prediction')
 user_df = input_data_streamlit(cost_of_living)
 
-if not user_df.empty:
-    st.write('Data Entered:')
-    st.write(user_df)
+# # if not user_df.empty:
+# #     st.write('Data Entered:')
+# #     st.write(user_df)
+
+# st.write('The expected salary for the selected features is approximately :')
+# # st.write(f'{user_df} €')
+
+# st.markdown("""
+# <style>
+# .big-font {
+#     font-size:50px !important;
+# }
+# </style>
+# """, unsafe_allow_html=True)
+
+# st.markdown(f'<p class="big-font">{user_df} €</p>', unsafe_allow_html=True)
+
